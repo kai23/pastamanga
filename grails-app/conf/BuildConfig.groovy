fbc924def857db71e330e692496c80dcf0d5cfc5
Florian CHEVALLIER <kai_23_@hotmail.Com>
2013-10-25 11:49:32 +0200

    ajout de bootstrap

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -71,5 +71,6 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
 		test ":spock:0.7"
+		compile ":twitter-bootstrap:3.0.0"
     }
 }
