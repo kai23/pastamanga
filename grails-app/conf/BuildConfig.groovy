27667c565d9895f32a3e82613395b35923fe13a3
uzerzero <clodagh.veale@gmail.com>
2013-10-25 12:03:56 +0200

    add spring security

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -71,5 +71,6 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
 		test ":spock:0.7"
+		compile ':spring-security-core:2.0-RC2'
     }
 }
